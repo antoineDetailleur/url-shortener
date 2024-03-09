7912394d35818fb0037a93de44edfb87ca89c3c2
antoineDetailleur <antoine.detailleur@protonmail.com>
2024-03-09 14:17:02 +0100

    Shorten url onClick

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,26 @@
 import Koa from "koa";
+import bodyParser from "koa-bodyparser"
+import cors from '@koa/cors'
+import { nanoid } from "nanoid";
 
 const app = new Koa();
 
+app.use(cors());
+
+app.use(bodyParser());
+
 app.use((ctx) => {
-  ctx.body = "Hello Koa";
+  var body = ctx.query;
+  var url = body.url as string;
+  const urlSplitted = url?.split("/");
+  const firstPart = urlSplitted[0];
+  var newUrl = "";
+  if (firstPart === "https:" || firstPart === "http:") {
+    newUrl = firstPart + "//" + urlSplitted[2] + "/" + nanoid()
+  } else {
+    newUrl = firstPart + "/" + nanoid()
+  }
+  ctx.body = newUrl;
 });
 
 app.listen(process.env.PORT, () => {
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,25 @@
 // API is available at import.meta.env.VITE_API_URL
+import axios from "axios";
 
 function App() {
+  const shortenUrl = async () => {
+    var url = (document.getElementById("url") as HTMLInputElement).value;
+    var newUrl = "";
+    await axios.get("http://localhost:3008/", { params: { url } }).then((response) => {
+      newUrl = response.data;
+    });
+    if (newUrl.length < url.length) {
+      (document.getElementById("url") as HTMLInputElement).value = newUrl;
+    }
+  }
+
   return (
     <main>
       <h1>URL shortener</h1>
-      <input />
-      <button>Shorten</button>
+      <div style={{ display: "flex" }}>
+        <textarea id="url" rows={10} style={{ flex: 1, resize: "none" }} />
+        <button onClick={shortenUrl}>Shorten</button>
+      </div>
     </main>
   );
 }
